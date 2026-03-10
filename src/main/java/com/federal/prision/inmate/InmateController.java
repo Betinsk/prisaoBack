b2388d96fe249cb1c03c0c054b37b1ea27d908e0
betinsk <gmdust@hotmail.com>
2026-03-10 20:20:54 -0300

    crude do inmate

--- src/main/java/com/federal/prision/inmate/InmateController.java
+++ src/main/java/com/federal/prision/inmate/InmateController.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.federal.prision.person.Person;
 
 @RestController
 @RequestMapping(value="inmate")
@@ -20,4 +24,19 @@ public class InmateController {
 	    List<Inmate> inmates = inmateService.findAll();
 	    return ResponseEntity.ok(inmates);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		Inmate inmate = inmateService.findById(id);
+		return ResponseEntity.ok().body(inmate);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteInmate(@PathVariable Long id) {
+		inmateService.deleteInmate(id);
+	return ResponseEntity.ok("Inmate deleted with success "+ id); 
+		                
+		}
+	
+	
 }
--- src/main/java/com/federal/prision/inmate/InmateService.java
+++ src/main/java/com/federal/prision/inmate/InmateService.java
@@ -3,7 +3,11 @@ package com.federal.prision.inmate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import com.federal.prision.resource.exceptions.DatabaseException;
+import com.federal.prision.resource.exceptions.ResourceNotFoundException;
 
 @Service
 public class InmateService {
@@ -15,4 +19,23 @@ public class InmateService {
 		return inmateRepository.findAll();
 	}
 	
+	public Inmate findById(Long id) {
+		return inmateRepository.findById(id)
+	            .orElseThrow(() -> new ResourceNotFoundException(
+	                    "Inmate not found. Id: " + id));
+	}
+	
+	
+	public void deleteInmate(Long id) {
+		if (!inmateRepository.existsById(id)) {
+	        throw new ResourceNotFoundException(
+	            "Entity not found. Id: " + id);
+	    }
+
+		 try {
+			 inmateRepository.deleteById(id);
+		    } catch (DataIntegrityViolationException e) {
+		        throw new DatabaseException("Integrity violation");
+		    }
+	}
 }
